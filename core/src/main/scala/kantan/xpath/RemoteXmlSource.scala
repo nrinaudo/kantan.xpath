60ea22e2186c99a254855fea89c13b4eb8f50d45
nrinaudo <nicolas@nrinaudo.com>
2016-02-05 22:35:39 +0100

    Documentation.

--- core/src/main/scala/kantan/xpath/RemoteXmlSource.scala
+++ core/src/main/scala/kantan/xpath/RemoteXmlSource.scala
@@ -4,6 +4,11 @@ import java.net.URL
 
 import org.xml.sax.InputSource
 
+/** [[XmlSource]] implementation anything that can be turned into a `java.io.URL`.
+  *
+  * The main purpose here is to allow application developers to set their own HTTP headers: when scrapping websites,
+  * it's typically necessary to change the default user agent to something a bit more browser-like.
+  */
 case class RemoteXmlSource[A](toURL: A => URL, headers: Map[String, String] = Map.empty)(implicit parser: XmlParser) extends XmlSource[A] {
   override def asNode(a: A): Option[Node] = {
     val con = toURL(a).openConnection()
@@ -14,6 +19,10 @@ case class RemoteXmlSource[A](toURL: A => URL, headers: Map[String, String] = Ma
 
   override def contramap[B](f: B => A): RemoteXmlSource[B] = copy(toURL = f andThen toURL)
 
+  /** Sets the specified header to the specified value. */
   def withHeader(name: String, value: String): RemoteXmlSource[A] =
     copy(headers = headers + (name -> value))
+
+  /** Sets the user-agent to use whenever connecting to a URL. */
+  def withUserAgent(value: String): RemoteXmlSource[A] = withHeader("User-Agent", value)
 }
--- core/src/main/scala/kantan/xpath/XmlParser.scala
+++ core/src/main/scala/kantan/xpath/XmlParser.scala
@@ -6,15 +6,30 @@ import org.xml.sax.InputSource
 
 import scala.util.Try
 
+/** Contract for anything that knows how to parse XML.
+  *
+  * The default implementation is always in scope and uses the standard java `javax.xml.parsers.DocumentBuilderFactory`,
+  * with no special option. This can be overridden to set some options or, more drastically, to change the parsing
+  * mechanism entirely. A typical use case is the `nekohtml` module, which needs to take control of the entire parsing
+  * process to tidy up messy HTML documents.
+  */
 trait XmlParser {
+  /** Turns the specified `InputSource` into a `Document`. */
   def parse(source: InputSource): Option[Document]
 }
 
+/** Declares the default [[kantan.xpath.XmlParser]] instance in the implicit scope. */
 object XmlParser {
+  /** Helper creation method, turns the specified function into an `Xmlparser`. */
   def apply(f: InputSource => Option[Document]): XmlParser = new XmlParser {
     override def parse(source: InputSource) = f(source)
   }
 
+  /** Default implementation.
+    *
+    * This relies on the standard java `javax.xml.parsers.DocumentBuilderFactory`, using whatever default parameters
+    * were set.
+    */
   implicit val builtIn: XmlParser = {
     val factory: DocumentBuilderFactory = DocumentBuilderFactory.newInstance()
     XmlParser(source => Try(factory.newDocumentBuilder().parse(source)).toOption)

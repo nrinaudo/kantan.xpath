3af5ecfe33a4085771ceab8d6ce166a327a141d4
nrinaudo <nicolas@nrinaudo.com>
2016-02-06 21:52:26 +0100

    Add every method to Expression.
    Work in progress, this might still be scrapped.

--- core/src/main/scala/kantan/xpath/Expression.scala
+++ core/src/main/scala/kantan/xpath/Expression.scala
@@ -2,7 +2,9 @@ package kantan.xpath
 
 import javax.xml.xpath.{XPathConstants, XPathExpression}
 
+import scala.annotation.tailrec
 import scala.collection.generic.CanBuildFrom
+import scala.collection.mutable
 
 class Expression private[xpath] (val expr: XPathExpression) {
   /** Finds the first node that matches the expression and evaluates it as an `A`. */
@@ -11,6 +13,22 @@ class Expression private[xpath] (val expr: XPathExpression) {
   /** Finds all nodes matching the expression and evaluate them as an `F[A]]`, where `F` is a collection class. */
   def all[F[_], A](n: Node)(implicit da: NodeDecoder[A], cbf: CanBuildFrom[Nothing, DecodeResult[A], F[DecodeResult[A]]]): F[DecodeResult[A]] =
     Expression.all(n, expr)(da.decode)
+
+  def every[F[_], A](n: Node)(implicit da: NodeDecoder[A], cbf: CanBuildFrom[Nothing, A, F[A]]): DecodeResult[F[A]] = {
+    val res = expr.evaluate(n, XPathConstants.NODESET).asInstanceOf[NodeList]
+
+    @tailrec
+    def loop(i: Int, acc: mutable.Builder[A, F[A]]): DecodeResult[F[A]] =
+      if(i >= res.getLength) DecodeResult.success(acc.result())
+      else da.decode(res.item(i)) match {
+        case DecodeResult.Success(a) =>
+          acc += a
+          loop(i + 1, acc)
+        case _ => DecodeResult.Failure
+      }
+
+    loop(0, cbf())
+  }
 
   /** Turns this expression into an unsafe one
     *

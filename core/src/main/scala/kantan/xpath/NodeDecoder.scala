70c2f73ceaaaf514cbf0cadadb96acd3fbf0d39f
nrinaudo <nicolas@nrinaudo.com>
2016-02-23 20:12:33 +0100

    Fix heavy-handed calls to apply.

--- core/src/main/scala/kantan/xpath/NodeDecoder.scala
+++ core/src/main/scala/kantan/xpath/NodeDecoder.scala
@@ -21,7 +21,7 @@ object NodeDecoder extends LowPriorityNodeDecoders with Decoders with TupleDecod
   }
 
   def safe[A](f: Node ⇒ A): NodeDecoder[A] = new NodeDecoder[A] {
-    override def decode(n: Node): EvaluationResult[A] = EvaluationResult.apply(f(n))
+    override def decode(n: Node): EvaluationResult[A] = EvaluationResult(f(n))
   }
 
   implicit val node: NodeDecoder[Node] = safe(n ⇒ n)
@@ -34,17 +34,17 @@ object NodeDecoder extends LowPriorityNodeDecoders with Decoders with TupleDecod
       else              s.charAt(0)
     }
   }
-  implicit val int: NodeDecoder[Int] = string.mapResult(s ⇒ EvaluationResult.apply(s.trim.toInt))
-  implicit val float: NodeDecoder[Float] = string.mapResult(s ⇒ EvaluationResult.apply(s.trim.toFloat))
-  implicit val double: NodeDecoder[Double] = string.mapResult(s ⇒ EvaluationResult.apply(s.trim.toDouble))
-  implicit val long: NodeDecoder[Long] = string.mapResult(s ⇒ EvaluationResult.apply(s.trim.toLong))
-  implicit val short: NodeDecoder[Short] = string.mapResult(s ⇒ EvaluationResult.apply(s.trim.toShort))
-  implicit val byte: NodeDecoder[Byte] = string.mapResult(s ⇒ EvaluationResult.apply(s.trim.toByte))
-  implicit val boolean: NodeDecoder[Boolean] = string.mapResult(s ⇒ EvaluationResult.apply(s.trim.toBoolean))
-  implicit val bigInt: NodeDecoder[BigInt] = string.mapResult(s ⇒ EvaluationResult.apply(BigInt(s.trim)))
-  implicit val bigDecimal: NodeDecoder[BigDecimal] = string.mapResult(s ⇒ EvaluationResult.apply(BigDecimal(s.trim)))
-  implicit val uuid: NodeDecoder[UUID] = string.mapResult(s ⇒ EvaluationResult.apply(UUID.fromString(s.trim)))
-  implicit val uri: NodeDecoder[URI] = string.mapResult(s ⇒ EvaluationResult.apply(new URI(s.trim)))
+  implicit val int: NodeDecoder[Int] = string.mapResult(s ⇒ EvaluationResult(s.trim.toInt))
+  implicit val float: NodeDecoder[Float] = string.mapResult(s ⇒ EvaluationResult(s.trim.toFloat))
+  implicit val double: NodeDecoder[Double] = string.mapResult(s ⇒ EvaluationResult(s.trim.toDouble))
+  implicit val long: NodeDecoder[Long] = string.mapResult(s ⇒ EvaluationResult(s.trim.toLong))
+  implicit val short: NodeDecoder[Short] = string.mapResult(s ⇒ EvaluationResult(s.trim.toShort))
+  implicit val byte: NodeDecoder[Byte] = string.mapResult(s ⇒ EvaluationResult(s.trim.toByte))
+  implicit val boolean: NodeDecoder[Boolean] = string.mapResult(s ⇒ EvaluationResult(s.trim.toBoolean))
+  implicit val bigInt: NodeDecoder[BigInt] = string.mapResult(s ⇒ EvaluationResult(BigInt(s.trim)))
+  implicit val bigDecimal: NodeDecoder[BigDecimal] = string.mapResult(s ⇒ EvaluationResult(BigDecimal(s.trim)))
+  implicit val uuid: NodeDecoder[UUID] = string.mapResult(s ⇒ EvaluationResult(UUID.fromString(s.trim)))
+  implicit val uri: NodeDecoder[URI] = string.mapResult(s ⇒ EvaluationResult(new URI(s.trim)))
   implicit val url: NodeDecoder[URL] = uri.map(u ⇒ u.toURL)
 
   implicit def either[A, B](implicit da: NodeDecoder[A], db: NodeDecoder[B]): NodeDecoder[Either[A, B]] = NodeDecoder { node ⇒

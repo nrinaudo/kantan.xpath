65ca8feda9c945946f95c9d33747abdc0654bc4a
nrinaudo <nicolas@nrinaudo.com>
2016-02-28 22:53:50 +0100

    Simpler implicits for Either tests.

--- cats/src/test/scala/kantan.xpath.cats/XorTests.scala
+++ cats/src/test/scala/kantan.xpath.cats/XorTests.scala
@@ -5,6 +5,7 @@ import kantan.codecs.laws.{IllegalString, LegalString}
 import codecs._
 import arbitrary._
 import cats.data.Xor
+import kantan.xpath.Node
 import kantan.xpath.laws.discipline.NodeDecoderTests
 import org.scalacheck.Arbitrary
 import org.scalatest.FunSuite
@@ -12,12 +13,8 @@ import org.scalatest.prop.GeneratorDrivenPropertyChecks
 import org.typelevel.discipline.scalatest.Discipline
 
 class XorTests extends FunSuite with GeneratorDrivenPropertyChecks with Discipline {
-  implicit def arbLegalXor(implicit a: Arbitrary[LegalString[Either[Int, Boolean]]]): Arbitrary[LegalString[Int Xor Boolean]] =
-    Arbitrary(a.arbitrary.map(_.mapDecoded(v ⇒ Xor.fromEither(v))))
-
-  implicit def arbIllegalXor(implicit a: Arbitrary[IllegalString[Either[Int, Boolean]]]): Arbitrary[IllegalString[Int Xor Boolean]] =
-    Arbitrary(a.arbitrary.map(_.mapDecoded(v ⇒ Xor.fromEither(v))))
-
+  implicit val legalXor = arbLegalNode[Int Xor Boolean]
+  implicit val illegalXor = arbIllegalNode[Int Xor Boolean]
 
   checkAll("NodeDecoder[Int Xor Boolean]", NodeDecoderTests[Int Xor Boolean].decoder[Int, Int])
 }
--- scalaz/src/test/scala/kantan/xpath/scalaz/DisjunctionTests.scala
+++ scalaz/src/test/scala/kantan/xpath/scalaz/DisjunctionTests.scala
@@ -2,9 +2,7 @@ package kantan.xpath.scalaz
 
 import codecs._
 import arbitrary._
-import kantan.codecs.laws.{IllegalString, LegalString}
 import kantan.xpath.laws.discipline.NodeDecoderTests
-import org.scalacheck.Arbitrary
 import org.scalatest.FunSuite
 import org.scalatest.prop.GeneratorDrivenPropertyChecks
 import org.typelevel.discipline.scalatest.Discipline
@@ -12,12 +10,8 @@ import org.typelevel.discipline.scalatest.Discipline
 import scalaz.\/
 
 class DisjunctionTests extends FunSuite with GeneratorDrivenPropertyChecks with Discipline {
-  // TODO: should this be moved into a kantan.codecs-laws-cats project? I'm bound to need to re-use it.
-  implicit def arbLegalDisjunction(implicit a: Arbitrary[LegalString[Either[Int, Boolean]]]): Arbitrary[LegalString[Int \/ Boolean]] =
-    Arbitrary(a.arbitrary.map(_.mapDecoded(v ⇒ \/.fromEither(v))))
-
-  implicit def arbIllegalDisjunction(implicit a: Arbitrary[IllegalString[Either[Int, Boolean]]]): Arbitrary[IllegalString[Int \/ Boolean]] =
-    Arbitrary(a.arbitrary.map(_.mapDecoded(v ⇒ \/.fromEither(v))))
+  implicit val legalDisjunction = arbLegalNode[Int \/ Boolean]
+  implicit val illegalDisjunction = arbIllegalNode[Int \/ Boolean]
 
   checkAll("NodeDecoder[Int \\/ Boolean]", NodeDecoderTests[Int \/ Boolean].decoder[Int, Int])
 }

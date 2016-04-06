04218f1cc4f11658f027a1518dcc3b97d60585ad
nrinaudo <nicolas@nrinaudo.com>
2016-04-06 19:13:03 +0200

    Add support for URI and URL.

--- tests/src/test/scala/kantan/xpath/URLDecoderTests.scala
+++ tests/src/test/scala/kantan/xpath/URLDecoderTests.scala
@@ -3,7 +3,6 @@ package kantan.xpath
 import java.net.URL
 import kantan.xpath.laws.discipline.NodeDecoderTests
 import kantan.xpath.laws.discipline.arbitrary._
-import org.scalacheck.{Arbitrary, Gen}
 import org.scalatest.FunSuite
 import org.scalatest.prop.GeneratorDrivenPropertyChecks
 import org.typelevel.discipline.scalatest.Discipline

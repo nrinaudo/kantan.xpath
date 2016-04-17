6e7356b6e4dbcd47e7ce5cf67613cdc9d9a4e0c1
nrinaudo <nicolas@nrinaudo.com>
2016-04-17 10:58:04 +0200

    Fix NodeDecoder for scala 2.10 - somehow the instance summoning method got marked as implicit, which was a bad idea.

--- core/src/main/scala/kantan/xpath/NodeDecoder.scala
+++ core/src/main/scala/kantan/xpath/NodeDecoder.scala
@@ -19,7 +19,7 @@ package kantan.xpath
 import kantan.codecs.Decoder
 
 object NodeDecoder extends GeneratedDecoders {
-  implicit def apply[A](implicit da: NodeDecoder[A]): NodeDecoder[A] = da
+  def apply[A](implicit da: NodeDecoder[A]): NodeDecoder[A] = da
 
   def apply[A](f: Node ⇒ DecodeResult[A]): NodeDecoder[A] = Decoder(f)
 }

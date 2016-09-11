da859fb752db40af30fdc0b6639d53e59f2c7ca2
nrinaudo <nicolas@nrinaudo.com>
2016-09-11 13:34:23 +0200

    Fix error tests (#8).

--- tests/src/test/scala/kantan/xpath/ErrorTests.scala
+++ tests/src/test/scala/kantan/xpath/ErrorTests.scala
@@ -23,7 +23,7 @@ import org.scalatest.FunSuite
 import org.scalatest.prop.GeneratorDrivenPropertyChecks
 
 class ErrorTests extends FunSuite with GeneratorDrivenPropertyChecks {
-  test("CompileErrors should be equal if the underlying exceptions are the same") {
+  test("CompileErrors should be equal if the underlying errors are the same") {
     forAll { (e1: CompileError, e2: XPathError) ⇒
       assert((e1 == e2) == ((e1, e2) match {
         case (CompileError(t1), CompileError(t2)) ⇒ t1 == t2
@@ -32,16 +32,13 @@ class ErrorTests extends FunSuite with GeneratorDrivenPropertyChecks {
     }
   }
 
-  test("CompileErrors should have identical hashCodes if the underlying exceptions are the same") {
-    forAll { (e1: CompileError, e2: XPathError) ⇒
-      assert((e1.hashCode() == e2.hashCode()) == ((e1, e2) match {
-        case (CompileError(t1), CompileError(t2)) ⇒ t1 == t2
-        case _                                    ⇒ false
-      }))
+  test("CompileErrors should have identical hashCodes if the underlying errors have the same hashCodes") {
+    forAll { (e1: CompileError, e2: CompileError) ⇒
+      assert((e1.hashCode() == e2.hashCode()) == (e1.message.hashCode == e2.message.hashCode))
     }
   }
 
-  test("TypeErrors should be equal if the underlying exceptions are the same") {
+  test("TypeErrors should be equal if the underlying errors are the same") {
     forAll { (e1: TypeError, e2: XPathError) ⇒
       assert((e1 == e2) == ((e1, e2) match {
         case (TypeError(t1), TypeError(t2)) ⇒ t1 == t2
@@ -50,16 +47,13 @@ class ErrorTests extends FunSuite with GeneratorDrivenPropertyChecks {
     }
   }
 
-  test("TypeErrors should have identical hashCodes if the underlying exceptions are the same") {
-    forAll { (e1: TypeError, e2: XPathError) ⇒
-      assert((e1.hashCode() == e2.hashCode()) == ((e1, e2) match {
-        case (TypeError(t1), TypeError(t2)) ⇒ t1 == t2
-        case _                              ⇒ false
-      }))
+  test("TypeErrors should have identical hashCodes if the underlying errors have the same hashCodes") {
+    forAll { (e1: TypeError, e2: TypeError) ⇒
+      assert((e1.hashCode() == e2.hashCode()) == (e1.message.hashCode == e2.message.hashCode))
     }
   }
 
-  test("SyntaxErrors should be equal if the underlying exceptions are the same") {
+  test("SyntaxErrors should be equal if the underlying errors are the same") {
     forAll { (e1: SyntaxError, e2: XPathError) ⇒
       assert((e1 == e2) == ((e1, e2) match {
         case (SyntaxError(t1), SyntaxError(t2)) ⇒ t1 == t2
@@ -68,16 +62,13 @@ class ErrorTests extends FunSuite with GeneratorDrivenPropertyChecks {
     }
   }
 
-  test("SyntaxErrors should have identical hashCodes if the underlying exceptions are the same") {
-    forAll { (e1: SyntaxError, e2: XPathError) ⇒
-      assert((e1.hashCode() == e2.hashCode()) == ((e1, e2) match {
-        case (SyntaxError(t1), SyntaxError(t2)) ⇒ t1 == t2
-        case _                                  ⇒ false
-      }))
+  test("SyntaxErrors should have identical hashCodes if the underlying errors have the same hashCodes") {
+    forAll { (e1: SyntaxError, e2: SyntaxError) ⇒
+      assert((e1.hashCode() == e2.hashCode()) == (e1.message.hashCode == e2.message.hashCode))
     }
   }
 
-  test("IOErrors should be equal if the underlying exceptions are the same") {
+  test("IOErrors should be equal if the underlying errors are the same") {
     forAll { (e1: IOError, e2: XPathError) ⇒
       assert((e1 == e2) == ((e1, e2) match {
         case (IOError(t1), IOError(t2)) ⇒ t1 == t2
@@ -86,12 +77,9 @@ class ErrorTests extends FunSuite with GeneratorDrivenPropertyChecks {
     }
   }
 
-  test("IOErrors should have identical hashCodes if the underlying exceptions are the same") {
-    forAll { (e1: IOError, e2: XPathError) ⇒
-      assert((e1.hashCode() == e2.hashCode()) == ((e1, e2) match {
-        case (IOError(t1), IOError(t2)) ⇒ t1 == t2
-        case _                          ⇒ false
-      }))
+  test("IOErrors should have identical hashCodes if the underlying errors have the same hashCodes") {
+    forAll { (e1: IOError, e2: IOError) ⇒
+      assert((e1.hashCode() == e2.hashCode()) == (e1.message.hashCode == e2.message.hashCode))
     }
   }
 }

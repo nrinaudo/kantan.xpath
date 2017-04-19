3d330fcfdae404d30c5a04c2b5366326d1182312
nrinaudo <nicolas@nrinaudo.com>
2017-04-19 23:08:29 +0200

    Upgrade to 2.11.11 and 2.12.2

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,6 +1,6 @@
 object Versions {
   val contextual    = "1.0.0"
-  val kantanCodecs  = "0.1.12"
+  val kantanCodecs  = "0.2.0-SNAPSHOT"
   val nekoHtml      = "1.9.22"
   val scalatest     = "3.0.1"
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,2 +1,2 @@
-addSbtPlugin("com.nrinaudo" % "kantan.sbt"      % "1.4.0")
+addSbtPlugin("com.nrinaudo" % "kantan.sbt"      % "1.4.2")
 addSbtPlugin("io.spray"     % "sbt-boilerplate" % "0.6.0")

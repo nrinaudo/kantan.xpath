d95c666ebc36a9098db5f99ecb05e4fd3a78f33b
nrinaudo <nicolas@nrinaudo.com>
2017-05-11 21:39:23 +0200

    Update to kantan.codecs 0.2.0.

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,6 +1,6 @@
 object Versions {
   val contextual    = "1.0.0"
-  val kantanCodecs  = "0.2.0-SNAPSHOT"
+  val kantanCodecs  = "0.2.0"
   val nekoHtml      = "1.9.22"
   val scalatest     = "3.0.3"
 }

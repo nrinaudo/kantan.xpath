188837312359f8ffb214f089845e3a5ec7a02dfb
Nicolas Rinaudo <nicolas@nrinaudo.com>
2018-01-09 22:35:41 +0100

    [#43] Update to kantan.sbt 2.0.0
    Fixes #43

--- project/Versions.scala
+++ project/Versions.scala
@@ -1,5 +1,5 @@
 object Versions {
-  val contextual    = "1.0.1"
-  val kantanCodecs  = "0.3.0"
-  val nekoHtml      = "1.9.22"
+  val contextual   = "1.0.1"
+  val kantanCodecs = "0.3.0"
+  val nekoHtml     = "1.9.22"
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,2 +1,1 @@
-addSbtPlugin("com.nrinaudo" % "kantan.sbt-boilerplate" % "1.7.6")
-addSbtPlugin("com.nrinaudo" % "kantan.sbt-kantan"      % "1.7.6")
+addSbtPlugin("com.nrinaudo" % "kantan.sbt-kantan" % "2.0.0")
